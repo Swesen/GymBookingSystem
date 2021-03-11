94236bd6fa69615e85e1e33b2b6949dfc43a73e2
Linus Boréus <swesen_xbox@live.se>
2021-03-11 11:42:11 +0100

    Created Equipment class

---
+++ Equipment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymBookingSystem
+{
+    class Equipment
+    {
+        public string Type { private set;  get; }
+        public int Id { private set; get; }
+        public string Status { private set; get; }
+        public bool Broken { private set; get; }
+
+        public  void SetAsBroken(string status)
+        {
+            Broken = true;
+            Status = status;
+        }
+    }
+}
